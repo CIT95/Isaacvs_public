7ace60c25aa45add92724c69dea2f12678921618
Paisita1 <isaacvirgen123@gmail.com>
2021-01-16 16:28:29 -0800

    Added comments

--- w2/interval_io.py
+++ w2/interval_io.py
@@ -1,17 +1,22 @@
 while True:
+	# get starting value
 	print('Enter a starting value.')
 	start_val = int(input())
 	print('')
 
+	# get ending value
 	print('Enter a ending value')
 	end_val = int(input())
 	print('')
 
+	# check if starting is bigger that ending
+	# starts loop again if true
 	if int(start_val) > int(end_val):
 		print('Error, starting value is greater than ending value.')
 		print('Please try again.\n')
 	else:
 		break
 
+#print numbers
 for i in range(int(start_val), int(end_val) + 1):
 	print(i)
